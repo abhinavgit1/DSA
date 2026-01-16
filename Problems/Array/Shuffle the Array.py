7b95786dd50c5dbce3b9c92c88bffa1f86d9abc6
abhinavgit1 <143893960+abhinavgit1@users.noreply.github.com>
2026-01-16 22:20:55 +0530

    Initial DSA structure with Problems folder new

--- Problems/Array/Shuffle the Array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def shuffle(self, nums: List[int], n: int) -> List[int]:
-        res = []
-        for i in range(n):
-            res.append(nums[i])
-            res.append(nums[n])
-            n+=1
-        return res
